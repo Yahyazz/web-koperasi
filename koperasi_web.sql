a30104c324e7bedf837893f414134325202f5eb6
Gunawan <92556282+Gunawann20@users.noreply.github.com>
2021-12-11 19:33:47 +0800

    Update koperasi_web.sql

--- koperasi_web.sql
+++ koperasi_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Des 2021 pada 12.33
+-- Waktu pembuatan: 11 Des 2021 pada 11.34
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.11
 
@@ -41,8 +41,36 @@ CREATE TABLE `anggota` (
 --
 
 INSERT INTO `anggota` (`id_anggota`, `nama`, `jenis_kelamin`, `alamat`, `tanggungan`) VALUES
-('Bud12345', 'Budi Sudarsono', 'Laki-laki', 'Sekaran', '2'),
+('AGS12345', 'Agus Samsudin', 'Laki-Laki', 'Sidareja', '2'),
+('And12345', 'Andi Cahyono', 'Laki-laki', 'Sidareja', '9'),
+('Ben12345', 'Beni', 'Laki-Laki', 'Gunungpati', '3'),
 ('Dev12345', 'Devira Norma Sari', 'Perempuan', 'Gunungpati', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `angsur`
+--
+
+CREATE TABLE `angsur` (
+  `kode_pinjaman` varchar(10) NOT NULL,
+  `id_anggota` varchar(10) NOT NULL,
+  `id_petugas` varchar(10) NOT NULL,
+  `tanggal_pembayaran` varchar(10) NOT NULL,
+  `angsuran_ke` varchar(10) NOT NULL,
+  `jumlah` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `angsur`
+--
+
+INSERT INTO `angsur` (`kode_pinjaman`, `id_anggota`, `id_petugas`, `tanggal_pembayaran`, `angsuran_ke`, `jumlah`) VALUES
+('P1001', 'Dev12345', 'PAGU123', '2021-12-22', '0', 400000),
+('P1001', 'Dev12345', 'PAGU123', '2021-12-22', '0', 400000),
+('P1001', 'Dev12345', 'PAGU123', '2021-12-22', '0', 400000),
+('P1002', 'AGS12345', 'PAGU123', '2021-12-30', '0', 200000),
+('P1004', 'Ben12345', 'PAGU123', '2021-12-24', '4', 240000);
 
 -- --------------------------------------------------------
 
@@ -54,9 +82,16 @@ CREATE TABLE `jaminan` (
   `kode_jaminan` varchar(10) NOT NULL,
   `jenis_jaminan` varchar(25) NOT NULL,
   `no_pemilik` varchar(25) NOT NULL,
-  `alamat` varchar(25) NOT NULL,
-  `kode_pinjaman` varchar(10) NOT NULL
+  `alamat` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jaminan`
+--
+
+INSERT INTO `jaminan` (`kode_jaminan`, `jenis_jaminan`, `no_pemilik`, `alamat`) VALUES
+('RMH-001', 'Rumah', '00020304450', 'Bumireja'),
+('RMH78', 'Rumah', '1234509976', 'Gunungpati');
 
 -- --------------------------------------------------------
 
@@ -67,7 +102,7 @@ CREATE TABLE `jaminan` (
 CREATE TABLE `menyimpan` (
   `id_anggota` varchar(10) NOT NULL,
   `kode_simpanan` varchar(10) NOT NULL,
-  `tahun` date NOT NULL,
+  `tanggal` varchar(20) NOT NULL,
   `jumlah` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -75,8 +110,9 @@ CREATE TABLE `menyimpan` (
 -- Dumping data untuk tabel `menyimpan`
 --
 
-INSERT INTO `menyimpan` (`id_anggota`, `kode_simpanan`, `tahun`, `jumlah`) VALUES
-('Bud12345', '001', '0000-00-00', 200000);
+INSERT INTO `menyimpan` (`id_anggota`, `kode_simpanan`, `tanggal`, `jumlah`) VALUES
+('Dev12345', '001', '10/12/2020', 1000000),
+('Dev12345', '002', '10/12/2021', 2000000);
 
 -- --------------------------------------------------------
 
@@ -89,9 +125,16 @@ CREATE TABLE `pelunasan` (
   `id_anggota` varchar(10) NOT NULL,
   `kode_jaminan` varchar(10) NOT NULL,
   `jumlah_pelunasan` int(15) NOT NULL,
-  `tgl_pelunasan` date NOT NULL,
+  `tgl_pelunasan` varchar(10) NOT NULL,
   `status` char(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pelunasan`
+--
+
+INSERT INTO `pelunasan` (`no_pelunasan`, `id_anggota`, `kode_jaminan`, `jumlah_pelunasan`, `tgl_pelunasan`, `status`) VALUES
+('2131341334', 'Dev12345', 'RMH78', 8000000, '2021-12-31', 'Lunas');
 
 -- --------------------------------------------------------
 
@@ -101,9 +144,9 @@ CREATE TABLE `pelunasan` (
 
 CREATE TABLE `penarikan` (
   `kode_penarikan` varchar(10) NOT NULL,
+  `id_anggota` varchar(10) NOT NULL,
   `no_transaksi` varchar(10) NOT NULL,
   `jml_penarikan` int(15) NOT NULL,
-  `jml_saldo` int(15) NOT NULL,
   `kode_simpanan` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -115,10 +158,19 @@ CREATE TABLE `penarikan` (
 
 CREATE TABLE `petugas` (
   `id_petugas` varchar(10) NOT NULL,
-  `nama` varchar(25) NOT NULL,
+  `nama_petugas` varchar(25) NOT NULL,
   `jenis_kelamin` varchar(25) NOT NULL,
   `bagian` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `jenis_kelamin`, `bagian`) VALUES
+('PAGU123', 'Agung Surono', 'Laki-Laki', 'Teller'),
+('PSAN123', 'Santoso Dwi', 'Laki-Laki', 'Kepala'),
+('PSUS123', 'Susi Lestari', 'Perempuan', 'Teller');
 
 -- --------------------------------------------------------
 
@@ -132,10 +184,20 @@ CREATE TABLE `pinjaman` (
   `lama_angsuran` int(5) NOT NULL,
   `bunga` float NOT NULL,
   `besar_angsuran` int(15) NOT NULL,
-  `no_angsuran` int(5) NOT NULL,
   `id_anggota` varchar(10) NOT NULL,
-  `id_petugas` varchar(10) NOT NULL
+  `id_petugas` varchar(10) NOT NULL,
+  `kode_jaminan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pinjaman`
+--
+
+INSERT INTO `pinjaman` (`kode_pinjaman`, `besar_pinjaman`, `lama_angsuran`, `bunga`, `besar_angsuran`, `id_anggota`, `id_petugas`, `kode_jaminan`) VALUES
+('P1001', 5000000, 36, 0.1, 140000, 'And12345', 'PSUS123', 'RMH-001'),
+('P1002', 10000000, 36, 0.1, 288000, 'AGS12345', 'PSUS123', 'RMH012'),
+('P1003', 8000000, 10, 0.1, 800000, 'Dev12345', 'PAGU123', 'RMH78'),
+('P1004', 8000000, 10, 0.1, 800000, 'Ben12345', 'PAGU123', 'RMH67');
 
 -- --------------------------------------------------------
 
@@ -168,18 +230,25 @@ ALTER TABLE `anggota`
   ADD PRIMARY KEY (`id_anggota`);
 
 --
+-- Indeks untuk tabel `angsur`
+--
+ALTER TABLE `angsur`
+  ADD KEY `kode_pinjaman` (`kode_pinjaman`),
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `id_anggota` (`id_anggota`);
+
+--
 -- Indeks untuk tabel `jaminan`
 --
 ALTER TABLE `jaminan`
-  ADD PRIMARY KEY (`kode_jaminan`),
-  ADD UNIQUE KEY `kode_pinjaman` (`kode_pinjaman`);
+  ADD PRIMARY KEY (`kode_jaminan`);
 
 --
 -- Indeks untuk tabel `menyimpan`
 --
 ALTER TABLE `menyimpan`
-  ADD KEY `id_anggota` (`id_anggota`),
-  ADD KEY `kode_simpanan` (`kode_simpanan`);
+  ADD KEY `menyimpan_ibfk_1` (`id_anggota`),
+  ADD KEY `menyimpan_ibfk_2` (`kode_simpanan`);
 
 --
 -- Indeks untuk tabel `pelunasan`
@@ -194,7 +263,8 @@ ALTER TABLE `pelunasan`
 --
 ALTER TABLE `penarikan`
   ADD PRIMARY KEY (`kode_penarikan`),
-  ADD UNIQUE KEY `kode_simpanan` (`kode_simpanan`);
+  ADD UNIQUE KEY `kode_simpanan` (`kode_simpanan`),
+  ADD KEY `penarikan_ibfk_1` (`id_anggota`);
 
 --
 -- Indeks untuk tabel `petugas`
@@ -207,8 +277,8 @@ ALTER TABLE `petugas`
 --
 ALTER TABLE `pinjaman`
   ADD PRIMARY KEY (`kode_pinjaman`),
-  ADD UNIQUE KEY `no_angsuran` (`no_angsuran`),
   ADD UNIQUE KEY `id_anggota` (`id_anggota`),
+  ADD UNIQUE KEY `kode_jaminan` (`kode_jaminan`),
   ADD KEY `pinjaman_ibfk_2` (`id_petugas`);
 
 --
@@ -222,37 +292,46 @@ ALTER TABLE `simpanan`
 --
 
 --
+-- Ketidakleluasaan untuk tabel `angsur`
+--
+ALTER TABLE `angsur`
+  ADD CONSTRAINT `angsur_ibfk_1` FOREIGN KEY (`kode_pinjaman`) REFERENCES `pinjaman` (`kode_pinjaman`),
+  ADD CONSTRAINT `angsur_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`),
+  ADD CONSTRAINT `angsur_ibfk_3` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`);
+
+--
 -- Ketidakleluasaan untuk tabel `jaminan`
 --
 ALTER TABLE `jaminan`
-  ADD CONSTRAINT `jaminan_ibfk_1` FOREIGN KEY (`kode_pinjaman`) REFERENCES `pinjaman` (`kode_pinjaman`);
+  ADD CONSTRAINT `jaminan_ibfk_1` FOREIGN KEY (`kode_jaminan`) REFERENCES `pinjaman` (`kode_jaminan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `menyimpan`
 --
 ALTER TABLE `menyimpan`
-  ADD CONSTRAINT `menyimpan_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
-  ADD CONSTRAINT `menyimpan_ibfk_2` FOREIGN KEY (`kode_simpanan`) REFERENCES `simpanan` (`kode_simpanan`);
+  ADD CONSTRAINT `menyimpan_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `menyimpan_ibfk_2` FOREIGN KEY (`kode_simpanan`) REFERENCES `simpanan` (`kode_simpanan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `pelunasan`
 --
 ALTER TABLE `pelunasan`
-  ADD CONSTRAINT `pelunasan_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
-  ADD CONSTRAINT `pelunasan_ibfk_2` FOREIGN KEY (`kode_jaminan`) REFERENCES `jaminan` (`kode_jaminan`);
+  ADD CONSTRAINT `pelunasan_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pelunasan_ibfk_2` FOREIGN KEY (`kode_jaminan`) REFERENCES `jaminan` (`kode_jaminan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `penarikan`
 --
 ALTER TABLE `penarikan`
-  ADD CONSTRAINT `penarikan_ibfk_1` FOREIGN KEY (`kode_simpanan`) REFERENCES `simpanan` (`kode_simpanan`);
+  ADD CONSTRAINT `penarikan_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `menyimpan` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `penarikan_ibfk_2` FOREIGN KEY (`kode_simpanan`) REFERENCES `menyimpan` (`kode_simpanan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `pinjaman`
 --
 ALTER TABLE `pinjaman`
-  ADD CONSTRAINT `pinjaman_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
-  ADD CONSTRAINT `pinjaman_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`);
+  ADD CONSTRAINT `pinjaman_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pinjaman_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
